f63330a7956e118b9b5e2d3aa6534f4999a6ebe1
bete7512 <betekbebe@gmail.com>
2023-01-11 21:10:07 +0300

    more on Mysql Advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
---my sql scripts that create users
-CREATE TABLE IF NOT EXISTS `users` (
-`id` INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`email` VARCHAR(255) NOT NULL UNIQUE,
-`name` VARCHAR(255),
-)
+-- SQL script that creates a table users
+CREATE TABLE If NOT EXISTS `users` (  
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `name` VARCHAR(255)
+);
