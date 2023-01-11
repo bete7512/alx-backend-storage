23b65b42b205f0f4286b9383919cbb5df98485f8
bete7512 <betekbebe@gmail.com>
2023-01-11 21:13:43 +0300

    more on Mysql Advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---my sql scripts that create users --
+-- my sql scripts that create users --
 CREATE TABLE IF NOT EXISTS `users` (
 `id` INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `email` VARCHAR(255) NOT NULL UNIQUE,
@@ -6,10 +6,4 @@ CREATE TABLE IF NOT EXISTS `users` (
 `country` ENUM('US','CO','TN') NOT NULL DEFAULT 'US'
 )
 
---  sql scripts that create users
-CREATE TABLE If NOT EXISTS `users` (
-`id` INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`email` VARCHAR(255) NOT NULL UNIQUE,
-`name` VARCHAR(255),
-`country` ENUM('US','CO','TN') NOT NULL DEFAULT='US'
-)
+
