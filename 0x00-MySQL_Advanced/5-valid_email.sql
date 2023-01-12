54c320b84c097d10c5080267e1a68022915f2515
bete7512 <betekbebe@gmail.com>
2023-01-12 09:53:17 +0300

    main thing

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,5 @@
 -- sql triggers for valid emails
-
+DELIMITER |
 CREATE TRIGGER valid_email_checker BEFORE UPDATE on users
 FOR EACH ROW 
 BEGIN
@@ -7,3 +7,4 @@ IF NEW.email <> OLD.email THEN
 SET NEW.valid_email = 0;
 ENDIF;
 END
+|
