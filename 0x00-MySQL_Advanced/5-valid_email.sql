c156502907f7c39f044549f9481a44fe64aceb81
bete7512 <betekbebe@gmail.com>
2023-01-12 09:51:04 +0300

    main thing

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- sql triggers for valid emails
+
+CREATE TRIGGER valid_email_checker BEFORE UPDATE on users
+FOR EACH ROW 
+BEGIN
+IF NEW.email <> OLD.email THEN 
+SET NEW.valid_email = 0;
+ENDIF;
+END
