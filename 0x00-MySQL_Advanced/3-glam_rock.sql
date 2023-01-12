7b0acc774d42170c5f2ae219334cb714026066de
bete7512 <betekbebe@gmail.com>
2023-01-12 09:10:25 +0300

    one more thing

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,11 @@
 -- sql script
-SELECT DISTINCT `band_name`,IFNULL(`split`,2023) - `formed` as `lifespan` 
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2023) - `formed` as `lifespan` 
 from `metal_bands` where FIND_IN_SET('Glam rock', style)
 ORDER by `lifespan` DESC;
+
+
+-- SELECT DISTINCT `band_name`,
+--                 IFNULL(`split`, 2020) - `formed` as `lifespan`
+--   FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+--   ORDER BY `lifespan` DESC;
